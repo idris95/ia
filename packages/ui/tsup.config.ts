d9fdea8ef68767438af906e56169169ce6043293
idris95 <idrisali725@gmail.com>
2024-01-10 14:30:08 +0600

    updated v0.0.5

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
+export default defineConfig((options) => ({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
@@ -11,10 +11,10 @@ export default defineConfig({
   treeshake: true,
   bundle: true,
   external: ["react", "react-dom"],
-  // does not support with splitting tsup v:7.2.0
-  // esbuildOptions(options) {
-  //   options.banner = {
-  //     js: '"use client"',
-  //   };
-  // },
-});
+  ...options,
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    };
+  },
+}));
